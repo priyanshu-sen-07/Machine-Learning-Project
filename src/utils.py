7a9433e3929adb8bae017de1f12d139aba9be443
priyanshu-sen-07 <priyanshusen96645@gmail.com>
2025-06-21 07:18:19 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import dill
 
 from src.exception import CustomException
 
@@ -59,3 +60,10 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(filepath):
+    try:
+        with open(filepath,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
